117b6182a1abf5a25e4c4d65eb21bbe316b423ed
ErinFlight <erinflight@gmail.com>
2020-06-09 18:19:28 -0500

    Created objects in Article

--- Article/Article.js
+++ Article/Article.js
@@ -111,3 +111,19 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+function articleMaker() {
+
+  const article = document.createElement('div');
+
+  const title = document.createElement('h3');
+
+  //Maybe should not have 3 here
+  const par1 = document.createElement('p');
+  const par2 = document.createElement('p');
+  const par3 = document.createElement('p');
+
+  const expandButton = document.createElement('span');
+
+
+}
